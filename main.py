c479ac7ffd5e086390ad6de6a09d2c1dfa59663b
obidano <omotetedan2@gmail.com>
2024-02-10 11:50:46 +0100

    feat: desactiver la dimension fixe de l'ecan pour eviter une erreur à la compilation de l'APK

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from kivy.core.window import Window
-from kivy.lang import Builder
+# from kivy.core.window import Window
+# from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, NoTransition
 from kivymd.app import MDApp
 
-Window.size = (350, 600)
+# Window.size = (350, 600)
 
 class WindowManager(ScreenManager):
     def route_initial(self):
