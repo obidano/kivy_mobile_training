885412a380e2d8e1a6d49355e46b9a018899bac8
obidano <omotetedan2@gmail.com>
2024-02-10 09:21:09 +0100

    feat: route initiale

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 from kivymd.app import MDApp
 
 Window.size = (350, 600)
 
 class WindowManager(ScreenManager):
-    pass
+    def route_initial(self):
+        self.transition = NoTransition()
+        self.current = "login"
 
 class MainApp(MDApp):
     def build(self):
         manager = WindowManager()
+        manager.route_initial()
         return manager
         # return Builder.load_file('main.kv')
 
