03470eb129cd4190c4af9b8df96efccb71182739
obidano <omotetedan2@gmail.com>
2024-02-10 11:45:40 +0100

    feat: login screen gestion des evenements utilisateur

--- user/login/login_screen.py
+++ user/login/login_screen.py
@@ -2,5 +2,15 @@ from kivymd.uix.screen import MDScreen
 
 
 class LoginScreen(MDScreen):
+    nom=""
+    mot_de_passe=""
+    def saisie_username(self, nom_clavier):
+        self.nom = nom_clavier
+        print(self.nom)
+    def saisie_mot_passe(self, mot_de_passe_clavier):
+        self.mot_de_passe= mot_de_passe_clavier
+    def valider(self):
+        print("Envoie du formulaire", self.nom, self.mot_de_passe)
+        self.manager.current = "etudiants"
     def on_enter(self, *args):
         pass
