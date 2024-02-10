e5009ca137d8e6c27d64e918ee61c13cced38296
obidano <omotetedan2@gmail.com>
2024-02-10 13:13:49 +0100

    feat: finalisation liste etudiants and recuperation des données avec faker

--- etudiants/liste_etudiants/liste_etudiants_screen.py
+++ etudiants/liste_etudiants/liste_etudiants_screen.py
@@ -2,5 +2,13 @@ from kivymd.uix.screen import MDScreen
 
 
 class ListeEtudiantsScreen(MDScreen):
+    def recuperer_etudiants(self):
+        from faker import Faker
+        faker=Faker()
+
+    def quand_la_page_sera_prete(self, *args):
+        self.ids.label_id.text = "Etudiants (1)"
+
     def on_enter(self, *args):
-        pass
+        from kivy.clock import Clock
+        Clock.schedule_once(self.quand_la_page_sera_prete, 0.5)
